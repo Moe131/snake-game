e18515fed399fc3a59949cfbbf8f212d190d316f
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-09-20 14:38:00 -0700

    snake board function created

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#define sq(x) x*x
 
 using namespace std;
 
+const int WIDTH = 50, HEIGHT = 25;
+
+void board(){
+    for (int i=0; i<HEIGHT ; i++){
+        cout << "\t\t#";
+        for (int j=0; j<WIDTH; j++){
+            if (i==0 || i== HEIGHT-1)
+                cout << "#";
+            else    
+                cout << " ";
+        }
+        cout << "#" << endl;
+    }
+} 
+
 int main(){
-    cout << "Hello World";
+    board();
     return 0;
 }
