8e58dec2c2bd8dc69f42d519c6cc3625b7e4c794
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-09-21 23:34:11 -0700

    snake changes direction using keyboard entry

--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
-#include <cstdlib>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <chrono>
 #include <thread>
 
 using namespace std;
 
 const int WIDTH = 50, HEIGHT = 25;
 
-int x = 0, y = 10;
+int x = 10, y = 10;
+string movement = "UP";
+
+// Function to check if a key was pressed
+int kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
 
 void board() {
-    system("clear");
+    cout << "\033[H"; // Move the cursor to the top-left corner
     for (int i = 0; i < HEIGHT; i++) {
         cout << "\t\t#";
         for (int j = 0; j < WIDTH; j++) {
@@ -24,14 +54,29 @@ void board() {
     }
 }
 
-// g++ main.cpp -o main
 int main() {
     while (true) {
         board();
-        x++;
-        if (x>25)
-            x=0;
-        this_thread::sleep_for(chrono::milliseconds(100)); // Pause for a short time
+        if (kbhit()) {
+            char ch = getchar();  // Get the pressed key
+            if (ch == 'w') 
+                movement = "UP";
+            else if (ch == 's') 
+                movement = "DOWN";
+            else if (ch == 'a') 
+                movement = "LEFT";            
+            else if (ch == 'd') 
+                movement = "RIGHT";
+        }
+        if (movement.compare("UP") == 0)
+            x--;
+        else if (movement.compare("DOWN") == 0)
+            x++;
+        else if (movement.compare("LEFT") == 0)
+            y-=2;
+        else if (movement.compare("RIGHT") == 0)
+            y+=2;
+        this_thread::sleep_for(chrono::milliseconds(200)); // Slow down the loop
     }
     return 0;
 }
