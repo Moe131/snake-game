f6f5a5fbef33a19ca92b44a4d2ab383362e560c9
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-09-21 23:18:25 -0700

    snake movement added

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
-#define sq(x) x*x
+#include <cstdlib>
+#include <thread>
 
 using namespace std;
 
 const int WIDTH = 50, HEIGHT = 25;
 
-void board(){
-    for (int i=0; i<HEIGHT ; i++){
+int x = 0, y = 10;
+
+void board() {
+    system("clear");
+    for (int i = 0; i < HEIGHT; i++) {
         cout << "\t\t#";
-        for (int j=0; j<WIDTH; j++){
-            if (i==0 || i== HEIGHT-1)
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == 0 || i == HEIGHT - 1)
                 cout << "#";
-            else    
+            else if (i == x && j == y)
+                cout << "*";
+            else
                 cout << " ";
         }
         cout << "#" << endl;
     }
-} 
+}
 
-int main(){
-    board();
+// g++ main.cpp -o main
+int main() {
+    while (true) {
+        board();
+        x++;
+        if (x>25)
+            x=0;
+        this_thread::sleep_for(chrono::milliseconds(100)); // Pause for a short time
+    }
     return 0;
 }
